817c6175e23b4c8a3d17a5d52a05227c0a20f6ee
NAMAN JAIN <53976820+itsNamanJain@users.noreply.github.com>
2021-10-04 19:57:27 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -28,7 +28,7 @@ function PlayGame(){  // Playing Game
         if(keys.ArrowUp && player.y>(road.top+180)){ player.y-=player.speed}
         if(keys.ArrowDown && player.y<(road.bottom-85)){ player.y+=player.speed}
         if(keys.ArrowLeft && player.x>0){ player.x-=player.speed}
-        if(keys.ArrowRight && player.x <(road.width-50)){ player.x+=player.speed}
+        if(keys.ArrowRight && player.x <(road.width-55)){ player.x+=player.speed}
 
         car.style.top = player.y+"px";
         car.style.left=player.x+"px";
