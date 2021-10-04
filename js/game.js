8b092ee41ace165e61c07ad52194609ca8c93166
NAMAN JAIN <53976820+itsNamanJain@users.noreply.github.com>
2021-10-04 19:49:57 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -63,6 +63,7 @@ function endGame(){
     player.start=false;
     popup.innerHTML="<p> Game Over <br> Your Last Score Was "+(player.scores+1)+" <br> Press Here to Play Again";
     player.scores=0;
+    player.speed=5;
     popup.classList.remove("hide");
 
 
